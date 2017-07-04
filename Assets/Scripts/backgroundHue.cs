90ce88b54de00350b87861085dbf73de7ea41559
Hüseyin Atakan Çiçek <cicekhu@gmail.com>
2017-07-04 16:15:52 +0300

    Initial commit

---
+++ Assets/Scripts/backgroundHue.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundHue : MonoBehaviour {
+
+    Camera cam;
+    int index = 0;
+    bool isGoingDark = false;
+    Color[] colorParam = { new Color(4f, 4f, 0f, 0f), new Color(0f, 4f, 4f, 0f), new Color(4f, 0f, 4f, 0f)};
+
+
+    // Use this for initialization
+    void Start () {
+        cam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        index = index % 3;
+
+        if (cam.backgroundColor.r >= 59f / 255f || cam.backgroundColor.g >= 59f / 255f || cam.backgroundColor.b >= 59f / 255f)
+            isGoingDark = true;
+
+        if(!isGoingDark)
+            cam.backgroundColor = cam.backgroundColor + (colorParam[index] / 4080);
+
+        if (cam.backgroundColor.r <= 35f / 255f && cam.backgroundColor.g <= 35f / 255f && cam.backgroundColor.b <= 35f / 255f)
+        {
+            isGoingDark = false;
+            index++;
+        }
+
+        if(isGoingDark)
+            cam.backgroundColor = cam.backgroundColor - (colorParam[index] / 4080);
+        
+    }
+}
---
+++ Assets/Scripts/ball.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour {
+
+    bool isDrag = false;
+    Vector3 mausePos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isDrag)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                mausePos = Input.mousePosition;
+                isDrag = true;
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButton(0))
+                Debug.Log("Duration");
+
+            else
+            {
+                isDrag = false;
+                GetComponent<Rigidbody2D>().velocity = (mausePos - Input.mousePosition) / 15;
+            }
+        }
+	}
+}
